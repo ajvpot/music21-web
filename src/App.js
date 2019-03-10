eb9be8a25ec79780e4510d90ab07c3e4806e4ed1
Alex Vanderpot <alex@vanderpot.com>
2019-03-10 18:32:10 -0400

    Python loader working, module imports broken

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {test} from './test.py';
+
 class App extends Component {
   render() {
     return (
@@ -10,6 +12,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
+            <br />
+            {test()}
           </p>
           <a
             className="App-link"
